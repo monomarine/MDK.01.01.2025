41b02904cff89595d7e9a137d84538ef660d3aac
sakdfalsf <sakdfalsf@gmail.com>
2025-09-19 18:42:16 +0500

    initial

--- DataStructures/Program.cs
+++ DataStructures/Program.cs
@@ -1,10 +1,21 @@
-﻿namespace DataStructures
+﻿using LinkedList;
+
+namespace DataStructures
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SLinkedList list = new SLinkedList();
+            list.AddFirst("1");
+            list.AddFirst("2");
+            list.AddFirst("3");
+            list.InsertAfter("2", "4");
+
+            foreach (var item in list)
+            {
+                Console.WriteLine(item);
+            }
         }
     }
 }
--- DataStructures/SLinkedList.cs
+++ DataStructures/SLinkedList.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace LinkedList
 {
 #pragma warning disable
-    internal class SLinkedList
+    internal class SLinkedList: IEnumerable<string>
     {
         private Node _head;
         private Node _tail;
@@ -29,18 +31,90 @@ namespace LinkedList
          * получить последний элемент
          * 
          * вставить элемент в начало списка
-         * добавить элемент в конец списка
+         * добавить элемент в конец списка!
          * добавить элемент после другого
          * 
-         * удалить первый элемент
-         * удалить последний элемент
-         * удалить элемент по его данным
+         * удалить первый элемент!
+         * удалить последний элемент!
+         * удалить элемент по его данным!
          * 
-         * проверить наличие элемента
-         * очистить список
+         * проверить наличие элемента!
+         * очистить список!
          * реализовать перебор элементов
-         * перевернуть
+         * перевернуть!
          * 
          */
-    }
+
+        public string GetFirst()
+        {
+            return _head == null ? null : _head.Data;
+        }
+
+        public string GetLast()
+        {
+            return _tail == null ? null : _tail.Data;
+        }
+
+        public void AddFirst(string text)
+        {
+            if (text == null)
+                throw new ArgumentException("данные пустые");
+
+            Node node = new(text);
+
+            if (IsEmpty)
+                _tail = node;
+            node.Next = _head;
+            _head = node;
+
+            _count++;
+        }
+
+        public void InsertAfter(string existingText, string text)
+        {
+            if (existingText == null || text == null)
+                throw new ArgumentException("данные пустые");
+            if (IsEmpty)
+            {
+                AddFirst(text);
+                return;
+            }
+
+            Node node = new(text);
+            Node current = _head;
+
+            while (current.Next != null)
+            {
+                if (current.Data == existingText)
+                {
+					node.Next = current.Next;
+					current.Next = node;
+                    _count++;
+                    break;
+                }
+                current = current.Next;
+            }
+
+        }
+
+        public void AddLast(string text)
+        {
+            throw new NotImplementedException();
+        }
+
+		public IEnumerator<string> GetEnumerator()
+		{
+            Node current = _head;
+            while (current != null)
+            {
+                yield return current.Data;
+                current = current.Next;
+            }
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+		    return GetEnumerator();
+		}
+	}
 }
