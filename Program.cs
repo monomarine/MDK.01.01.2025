9ceaedae5d2890d46d485df9f4b748bc13ff9601
Koretskaya Alisa <156618535+lirisme@users.noreply.github.com>
2025-09-22 23:56:10 +0500

    Add files via upload
    добавила методы для получения последнего элемента, удаления первого, удаления по данным и просмотра последнего

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace Stack
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                StackOnArray stack = new StackOnArray();
+                stack.Push("Москва");
+                stack.Push("Екатеринбург");
+                stack.Push("Оренбург");
+                stack.Push("Самара");
+
+                stack.Push("Орск");
+
+                Console.WriteLine("последний элемент: " + stack.Peek());
+
+                //Console.WriteLine(stack.Pop()) ;
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ SLinkedList.cs
@@ -0,0 +1,244 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+#pragma warning disable
+    internal class SLinkedList : IEnumerable<string>
+    {
+        private Node _head;
+        private Node _tail;
+        private int _count;
+
+        public int Count => _count;
+        public bool IsEmpty => _head == null;
+
+        public SLinkedList()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+        /*
+         * функционал:
+         * 
+         * получить первый элемент !
+         * получить последний элемент - сам +
+         * 
+         * удалить первый элемент - сам +
+         * удалить последний элемент !
+         * удалить элемент по его данным - сам +
+         * 
+         * проверить наличие элемента !
+         * перевернуть!
+         * 
+         */
+
+        //получение последнего элемента
+        public string GetLast()
+        {
+            if (IsEmpty)
+                throw new ArgumentNullException("список пуст");
+            return _tail.Data;
+        }
+
+        //удаление последнего
+        public void RemoveFirst()
+        {
+            if (IsEmpty)
+                throw new ArgumentNullException("список пуст");
+            else if (Count == 1)
+                Clear();
+            else
+            {
+                _head = _head.Next;
+                _count--;
+            }
+
+        }
+
+        //удаление по данным
+        public void RemoveData(string data)
+        {
+            if (IsEmpty)
+                throw new ArgumentNullException("список пуст");
+            if (data == null)
+                throw new ArgumentException("данные отсутствувют");
+            if (_head.Data == data)
+            {
+                RemoveFirst();
+                return;
+            }
+
+            Node current = _head;
+
+            while (current != null)
+            {
+                if (current.Next.Data == data)
+                    if (current.Next == _tail)
+                    {
+                        _tail = current;
+                    }
+                current = current.Next.Next;
+
+                _count--;
+                return;
+            }
+            current = current.Next;
+
+            throw new InvalidOperationException($"данного элемента нет в списке");
+        }
+
+        public void Reverse()
+        {
+            if (Count <= 1)
+                throw new ArgumentException("количество элементов не достаточно для реверса");
+
+            Node prev = null;
+            Node current = _head;
+            Node next = null;
+
+            _tail = _head;
+
+            while (current != null)
+            {
+                next = current.Next;
+                current.Next = prev;
+                prev = current;
+                current = next;
+            }
+            _head = prev;
+        }
+        public void RemoveLast()
+        {
+            if (IsEmpty)
+                throw new ArgumentNullException("список пуст");
+            else if (Count == 1)
+                Clear();
+            else
+            {
+                Node current = _head;
+                while (current.Next != _tail)
+                {
+                    current = current.Next;
+                }
+                current.Next = null;
+                _tail = current;
+
+                _count--;
+            }
+
+
+        }
+
+        public string GetFirst()
+        {
+            if (IsEmpty)
+                throw new ArgumentNullException("список пуст");
+            else
+                return _head.Data;
+        }
+
+        public bool Contains(string item)
+        {
+            if (IsEmpty)
+                throw new ArgumentNullException("список пуст");
+            else
+            {
+                Node current = _head;
+                while (current.Next != null)
+                {
+                    if (current.Data == item)
+                    {
+                        return true;
+                    }
+                    current = current.Next;
+                }
+                return false;
+            }
+        }
+        public void AddFirst(string text)
+        {
+            if (text == null)
+                throw new ArgumentException("данные пустые");
+            Node newNode = new Node(text);
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                newNode.Next = _head;
+                _head = newNode;
+            }
+            _count++;
+        }
+
+        public void AddLast(string text)
+        {
+            if (text == null)
+                throw new ArgumentException("данные пустые");
+            Node newNode = new Node(text);
+            if (_head == null)
+            {
+                _head = newNode;
+                _tail = newNode;
+            }
+            else
+            {
+                _tail.Next = newNode;
+                _tail = newNode;
+            }
+            _count++;
+        }
+
+        public void InsertAfter(string existingText, string text)
+        {
+            if (existingText == null || text == null)
+                throw new ArgumentException("данные не могут быть пустые");
+            Node newNode = new Node(text);
+            Node current = _head;
+            while (current.Next != null)
+            {
+                if (current.Data == existingText)
+                {
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                    if (current == _tail)
+                        _tail = current;
+                }
+                current = current.Next;
+            }
+
+            _count++;
+        }
+
+        public IEnumerator<string> GetEnumerator()
+        {
+            Node current = _head;
+            while (current != null)
+            {
+
+                yield return current.Data;
+                current = current.Next;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Clear()
+        {
+            _head = null;
+            _tail = null;
+            _count = 0;
+        }
+    }
+}
