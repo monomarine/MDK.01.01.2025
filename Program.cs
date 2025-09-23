8a389689296115a5aa181ce125c0bdad40487fe2
Koretskaya Alisa <156618535+lirisme@users.noreply.github.com>
2025-09-23 19:00:25 +0500

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,34 +1,57 @@
-﻿namespace Stack
+﻿using System.Collections.Immutable;
+using System.Net.WebSockets;
+
+namespace Search
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            try
+            int key = 64655;
+            int[] sizes = { 100, 100000, 3000000 };
+
+            Console.WriteLine("КЛЮЧ В КОНЦЕ ПОСЛЕДОВАТЕЛЬНОСТИ");
+            foreach (int size in sizes)
             {
-                StackOnArray stack = new StackOnArray();
-                stack.Push("Москва");
-                stack.Push("Екатеринбург");
-                stack.Push("Оренбург");
-                stack.Push("Самара");
-
-                stack.Push("Орск");
-
-                Console.WriteLine("последний элемент: " + stack.Peek());
-
-                //Console.WriteLine(stack.Pop()) ;
-
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e.Message);
+                Console.WriteLine($"\n размер массива: {size}");
+                int[] data = GetArray(size, key, true);
+                Search.LinearSearch(key, data);
+                Search.IterativeBinarySearch(key, data);
+                Search.InterpolateSearch(key, data);
             }
 
-
-
-
-
-
+            Console.WriteLine("\n КЛЮЧ В СЛУЧЙНОМ МЕСТЕ ПОСЛЕДОВАТЕЛЬНОСТИ");
+            foreach (int size in sizes)
+            {
+                Console.WriteLine($"\n размер массива: {size}");
+                int[] data = GetArray(size, key, false);
+                Search.LinearSearch(key, data);
+                Search.IterativeBinarySearch(key, data);
+                Search.InterpolateSearch(key, data);
+            }
         }
+
+        static int[] GetArray(int count, int key, bool keyAtEnd = true)
+        {
+            int[] array = new int[count];
+            Random rand = new Random();
+            for (int i = 0; i < count; i++)
+            {
+                array[i] = rand.Next(0, key);
+            }
+            if (keyAtEnd)
+            {
+                array[count - 1] = key;
+            }
+            else
+            {
+                int randomIndex = rand.Next(0, count);
+                array[randomIndex] = key;
+            }
+
+            Array.Sort(array);
+            return array;
+        }
+       
     }
 }
