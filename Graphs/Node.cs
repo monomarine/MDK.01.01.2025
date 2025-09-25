de8af6f2b870ea124b29ff706e109209be88bb29
sakdfalsf <sakdfalsf@gmail.com>
2025-09-25 13:51:06 +0500

    Добавлен граф для хранения студентов

---
+++ Graphs/Node.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+	internal class Node
+	{
+		public Student Value { get; set; }
+		public List<Node> Neighbors { get; set; }
+
+		public Node(Student value)
+		{
+			Value = value;
+			Neighbors = new List<Node>();
+		}
+
+		public override string ToString() => Value.FullName;
+	}
+}
--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -6,14 +6,32 @@ namespace Graph
     {
         public static void Main(string[] args)
         {
-			int[,] a = { { 0,0,0,0},
+			/*int[,] a = { { 0,0,0,0},
 						 { 0,0,1,0},
 						 { 1,0,0,0},
 						 { 1,0,1,0}
 						};
 
 			GraphByMatrix graph = new GraphByMatrix(a);
-			graph.Depth(1);
+			graph.Depth(1); */
+			Student rootStudent = new Student("Иванов Иван Сергеевич", 5.0f);
+			Student secondStudent = new Student("Сергеев Сергей Иванович", 3.8f);
+			Student thirdStudent = new Student("Геннадьев Антон Викторович", 4.5f);
+			Student fourthStudent = new Student("Куров Виктор Антонович", 4.0f);
+			Student fifthStudent = new Student("Конев Александр Александрович", 4.5f);
+			GraphByList graph = new(rootStudent);
+			
+			Node first = graph.AddNode(secondStudent);
+			Node second = graph.AddNode(thirdStudent, first);
+			Node third = graph.AddNode(fourthStudent, second);
+			Node fourth = graph.AddNode(fifthStudent, second);
+
+			Console.WriteLine("= Обход в ширину =");
+			graph.Width();
+			Console.WriteLine("=============");
+			Console.WriteLine($"Общее количество баллов => {graph.ReceiveFullScore()}");
+			Console.WriteLine($"Среднее количество баллов => {graph.ReceiveAverageScore()}");
+			Console.WriteLine($"Самый общительный студент => {graph.FindNodeWithMostMembers()}");
 		}
     }
 }
