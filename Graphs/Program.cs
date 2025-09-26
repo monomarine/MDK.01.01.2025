b41a48327f2b60fc44148e2a3e1d364006781da9
catalog <vashhenko.vitalik.2007@mail.ru>
2025-09-26 12:09:11 +0500

    Solution

--- Graphs/Program.cs
+++ Graphs/Program.cs
@@ -4,17 +4,9 @@ namespace Graph
 {
     internal class Program
     {
+        /* 
         static void Main(string[] args)
         {
-            /*int[,] a = { { 0,0,0,0},
-                         { 0,0,1,0},
-                         { 1,0,0,0},
-                         { 1,0,1,0}
-                        };
-
-            GraphByMatrix graph = new GraphByMatrix(a);
-            graph.Depth(0);*/
-
             GraphByList graph = new GraphByList("Москва");
             Node n1 = graph.AddNode("Санкт-Петербург");
             Node n2 = graph.AddNode("Оренбург", n1);
@@ -32,5 +24,6 @@ namespace Graph
 
             graph.Depht();
         }
+        */
     }
 }
