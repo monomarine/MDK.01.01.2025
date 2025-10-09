b29befea4a4991722d12a8857f4df36518f6ccc1
Pushechnikova Valeria <PLera2020@yandex.ru>
2025-10-09 13:21:07 +0500

    Задание сборщик мусора

---
+++ Garbage/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Diagnostics;
+
+namespace Garbage
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Анализ сборщика мусора");
+
+            long initialMemory = GC.GetTotalMemory(false);
+            Console.WriteLine($"Начальная память: {initialMemory} байт");
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            Analysis.TestSmallCollection();
+
+            Analysis.TestLargeCollection();
+
+            Analysis.TestCollectionWithChanges();
+
+            stopwatch.Stop();
+            
+            long finalMemory = GC.GetTotalMemory(true);
+            Console.WriteLine($"\nФинальная память: {finalMemory} байт");
+            Console.WriteLine($"Общее время выполнения: {stopwatch.ElapsedMilliseconds} мс");
+            
+            Analysis.PrintGCStats();
+        }
+    }
+}
