ddfc57bfd9cdeeee5c3076f53d3fdb71b4c4bfaf
Azingamot <159550570+Azingamot@users.noreply.github.com>
2025-10-10 17:48:07 +0500

    Добавлена фильтрация строк

--- 02.04_Delegates/Program.cs
+++ 02.04_Delegates/Program.cs
@@ -1,30 +1,38 @@
 ﻿namespace _02._04_Delegates
 {
-    public delegate bool FilterDelegate(int x);
+    public delegate bool FilterDelegate<T>(T x);
     internal class Program
     {
         //переписать код для коллекции строк и произвести фильтрацию строк (вывести в результат только
         //те строки, в которых нет спецсимволов)
         static void Main(string[] args)
         {
-            List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
-            var res1 = Filter(numbers, n => n % 2 == 0);
-            var res2 = Filter(numbers, n => n > 10);
+            List<string> strings = new() {"Имя", "!Имя", "&Имя", "***ТЕКСТ****", "Текст", "Да"};
 
+            var res = Filter<string>(strings, n => !n.Any(ch => !char.IsLetterOrDigit(ch)));
 
-            foreach(var r in res2)
-                Console.Write(r+ " ");
-        } 
-
-        public static List<int> Filter(List<int> data, FilterDelegate delegat)
-        {
-            var result = new List<int>();
-            foreach (int d in data)
-            {
-                if (delegat(d))
-                    result.Add(d);
-            }
-            return result;
+            Console.WriteLine(string.Join(", ", res));
         }
-    }
+
+		public static List<T> Filter<T>(List<T> data, FilterDelegate<T> delegat)
+		{
+			var result = new List<T>();
+			foreach (T x in data)
+			{
+				if (delegat(x))
+					result.Add(x);
+			}
+			return result;
+		}
+
+        private static bool CheckString(string text, char[] filter)
+        {
+            foreach (var x in filter)
+            {
+                if (text.Contains(x))
+                    return false;
+            }
+            return true;
+        }
+	}
 }
