9dde1369fd8be2a8968a7189b5bb37a530214b35
Mariya <778meri780@gmail.com>
2025-10-10 18:30:35 +0500

    newCommit

--- 02.04_Delegates/Program.cs
+++ 02.04_Delegates/Program.cs
@@ -1,60 +1,42 @@
-﻿using System.Security.Cryptography.X509Certificates;
+﻿using System;
+using System.Collections.Generic;
 
 namespace _02._04_Delegates
 {
-    public delegate bool FilterDelegate(int x);
-    public delegate bool FilterStringsDelegate(string text);
+    public delegate bool FilterDelegate(string x);
+
     internal class Program
     {
-        //переписать код для коллекции строк и произвести фильтрацию строк (вывести в результат только
-        //те строки, в которых нет спецсимволов)
         static void Main(string[] args)
         {
-            List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
-            List<string> strings = new() { "user", "admin$", "гость"};
-            var res1 = Filter(strings, text =>
-            {
-                foreach (var r in text)   
-                {
-                    if (!char.IsLetterOrDigit(r)) 
-                    return false;
-                }
-                return true;
-            });
+            List<string> words = new() { "Hello", "@World_", "123", "jcfj" };
 
-            foreach(var r in res1)
+            var filtered = Filter(words, s => IsAlphanumeric(s));
+
+            foreach (var word in filtered)
             {
-                Console.Write(r+ " ");
+                Console.Write(word + " ");
             }
-            //var res1 = Filter(numbers, n => n % 2 == 0);
-            //var res2 = Filter(numbers, n => n > 10);
-
-
-
-            //foreach(var r in res2)
-            //Console.Write(r+ " ");
         }
 
-        public static List<int> Filter(List<int> data, FilterDelegate delegat)
+        public static List<string> Filter(List<string> data, FilterDelegate delegat)
         {
-            var result = new List<int>();
-            foreach (int d in data)
+            var result = new List<string>();
+            foreach (string d in data)
             {
                 if (delegat(d))
                     result.Add(d);
             }
             return result;
         }
-
-        public static List<string> Filter(List<string> data, FilterStringsDelegate delegat)
+        public static bool IsAlphanumeric(string str)
         {
-            var resultstrings = new List<string>();
-            foreach (string d in data)
+            foreach (char c in str)
             {
-                if (delegat(d))
-                    resultstrings.Add(d);
+                if (!char.IsLetterOrDigit(c))
+                    return false;
             }
-            return resultstrings;
+            return true;
         }
     }
 }
