f07c5fdd24fc5fb00fe6e56d954efb4ba498ef37
Хасанова Динара <khasanova.de2749@okeit.edu>
2025-10-10 18:29:13 +0500

    реализована фильтрация строк

--- 02.04_Delegates/Program.cs
+++ 02.04_Delegates/Program.cs
@@ -1,28 +1,41 @@
 ﻿namespace _02._04_Delegates
 {
-    public delegate bool FilterDelegate(int x);
+    public delegate bool FilterDelegate(string x);
+    
     internal class Program
     {
         //переписать код для коллекции строк и произвести фильтрацию строк (вывести в результат только
         //те строки, в которых нет спецсимволов)
         static void Main(string[] args)
         {
-            List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
-            var res1 = Filter(numbers, n => n % 2 == 0);
-            var res2 = Filter(numbers, n => n > 10);
+            //List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
+            //var res1 = Filter(numbers, n => n % 2 == 0);
+            //var res2 = Filter(numbers, n => n > 10);
 
+            List<string> text = new() { "one", "two", "alisa", "b@#$ook", "pig5" };
+            var res3 = Filter(text, ttext =>
+            {
+                foreach (var r in ttext)
+                {
+                    if (!char.IsLetterOrDigit(r))
+                    return false;
+                }
+                return true;
+            } 
+            );
 
-            foreach(var r in res2)
-                Console.Write(r+ " ");
+            foreach (var r in res3)
+            {
+                Console.Write(r + " ");
+            }
         } 
 
-        public static List<int> Filter(List<int> data, FilterDelegate delegat)
+        public static List<string> Filter(List<string> data, FilterDelegate delegat)
         {
-            var result = new List<int>();
-            foreach (int d in data)
+            var result = new List<string>();
+            foreach (string d in data)
             {
-                if (delegat(d))
-                    result.Add(d);
+                if (delegat(d)) result.Add(d);
             }
             return result;
         }
