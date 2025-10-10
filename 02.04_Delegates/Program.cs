663643fbb829ec453b733f63996514fd5fa9d425
danilova veronika <dveronika682@gmail.com>
2025-10-10 18:10:31 +0500

    фильтрация строк

--- 02.04_Delegates/Program.cs
+++ 02.04_Delegates/Program.cs
@@ -1,6 +1,11 @@
-﻿namespace _02._04_Delegates
+﻿using System;
+using System.Collections.Generic;
+
+namespace _02._04_Delegates
 {
     public delegate bool FilterDelegate(int x);
+    public delegate bool FilterStringDelegate(string text);
+
     internal class Program
     {
         //переписать код для коллекции строк и произвести фильтрацию строк (вывести в результат только
@@ -8,22 +13,37 @@
         static void Main(string[] args)
         {
             List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
-            var res1 = Filter(numbers, n => n % 2 == 0);
-            var res2 = Filter(numbers, n => n > 10);
+            List<string> strings = new() { "angelina", "karina", "alisa", "al!sa", "masha", "vasya__pupkin" };
 
+            var res = Filter(strings, text =>
+            {
+                foreach (char q in text)
+                {
+                    if (!(char.IsLetterOrDigit(q) || q == ' '))
+                        return false;
+                }
+                return true;
+            });
 
-            foreach(var r in res2)
-                Console.Write(r+ " ");
-        } 
+            foreach (var q in res)
+                Console.WriteLine(q);
+        }
 
         public static List<int> Filter(List<int> data, FilterDelegate delegat)
         {
             var result = new List<int>();
             foreach (int d in data)
-            {
                 if (delegat(d))
                     result.Add(d);
-            }
+            return result;
+        }
+
+        public static List<string> Filter(List<string> data, FilterStringDelegate delegat)
+        {
+            var result = new List<string>();
+            foreach (string s in data)
+                if (delegat(s))
+                    result.Add(s);
             return result;
         }
     }
