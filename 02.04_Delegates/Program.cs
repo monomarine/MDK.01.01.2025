c685877a7e98b4b61c7e52a364411f4170f3c01e
Ruzaki075 <chernevigor78@gmail.com>
2025-10-10 18:27:08 +0500

    Чернев

--- 02.04_Delegates/Program.cs
+++ 02.04_Delegates/Program.cs
@@ -1,25 +1,55 @@
 ﻿namespace _02._04_Delegates
 {
-    public delegate bool FilterDelegate(int x);
+    //public delegate bool FilterDelegate(int x);
+    public delegate bool FilterStringDelegate(string text);
+
     internal class Program
     {
-        //переписать код для коллекции строк и произвести фильтрацию строк (вывести в результат только
-        //те строки, в которых нет спецсимволов)
         static void Main(string[] args)
         {
-            List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
-            var res1 = Filter(numbers, n => n % 2 == 0);
-            var res2 = Filter(numbers, n => n > 10);
+            //List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5 };
+            //var res1 = Filter(numbers, n => n % 2 == 0);
+            //var res2 = Filter(numbers, n => n > 10);
 
+            List<string> strings = new()
+            {
+                "Serega", "Maksim!", "Artemkotik@@@", "IgorRuzaki", "qwe---rty", "@Chernev", "qqqqq", "zxcvb"
+            };
 
-            foreach(var r in res2)
-                Console.Write(r+ " ");
-        } 
+            var filteredStrings = FilterStrings(strings, text =>
+            {
+                foreach (var r in text)
+                {
+                    if (!char.IsLetterOrDigit(r) && !char.IsWhiteSpace(r))
+                        return false;
+                }
+                return true;
+            });
 
-        public static List<int> Filter(List<int> data, FilterDelegate delegat)
+            //Console.WriteLine("Числа больше 10:");
+            //foreach (var r in res2)
+            //    Console.Write(r + " ");
+
+            Console.WriteLine("Строки без спец символов:");
+            foreach (var r in filteredStrings)
+                Console.WriteLine(r);
+        }
+
+        //public static List<int> Filter(List<int> data, FilterDelegate delegat)
+        //{
+        //    var result = new List<int>();
+        //    foreach (int d in data)
+        //    {
+        //        if (delegat(d))
+        //            result.Add(d);
+        //    }
+        //    return result;
+        //}
+
+        public static List<string> FilterStrings(List<string> data, FilterStringDelegate delegat)
         {
-            var result = new List<int>();
-            foreach (int d in data)
+            var result = new List<string>();
+            foreach (string d in data)
             {
                 if (delegat(d))
                     result.Add(d);
