002d219e98ee89645ef3d3b4aa570f68f3122994
Alice <gdweyuwyh23gmail.com>
2025-10-10 18:01:33 +0500

    ку

--- 02.04_Delegates/Program.cs
+++ 02.04_Delegates/Program.cs
@@ -1,6 +1,8 @@
 ﻿namespace _02._04_Delegates
 {
     public delegate bool FilterDelegate(int x);
+    public delegate bool FilterStringDelegate(string text);
+
     internal class Program
     {
         //переписать код для коллекции строк и произвести фильтрацию строк (вывести в результат только
@@ -8,18 +10,30 @@
         static void Main(string[] args)
         {
             List<int> numbers = new() { 4, 8, 2, 8, 5, 98, 8, 5, };
-            var res1 = Filter(numbers, n => n % 2 == 0);
-            var res2 = Filter(numbers, n => n > 10);
+            List<string> strings = new()
+            {
+                "Hello", "World!", "Test@123","Normalniy text", "koreckaaalisa4@.com", "@lirisme","  k", "pupupuuu"
+            };
 
+            var res1 = FilterStrings(strings, text =>
+            {
+                foreach (var r in text)
+                {
+                    if (!char.IsLetterOrDigit(r) && !char.IsWhiteSpace(r))
+                        return false;
+                }
+                return true;
+            });
 
-            foreach(var r in res2)
-                Console.Write(r+ " ");
-        } 
+            Console.WriteLine("Строки без спец символов:");
+            foreach (var r in res1)
+                Console.WriteLine(r);
+        }
 
-        public static List<int> Filter(List<int> data, FilterDelegate delegat)
+        public static List<string> FilterStrings(List<string> data, FilterStringDelegate delegat)
         {
-            var result = new List<int>();
-            foreach (int d in data)
+            var result = new List<string>();
+            foreach (string d in data)
             {
                 if (delegat(d))
                     result.Add(d);
