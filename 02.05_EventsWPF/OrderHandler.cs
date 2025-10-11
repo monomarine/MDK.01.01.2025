4c34237ea704d19af1b29e99079d9f7e394d9883
Eset <maltabaroveset@gmail.com>
2025-10-11 15:45:08 +0500

    Утяпов Есет

---
+++ 02.05_EventsWPF/OrderHandler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace _02._05_EventsWPF
+{
+    internal class OrderHandler
+    {
+        private readonly string _logFile = "logs.txt";
+
+
+        public void LogOrderPayment(Order order, OrderEventArgs e)
+        {
+            string orderInfo = $"оплата от заказчика {order.Client} по заказу номер {order.Id} на сумму {e.Summ}";
+            File.AppendAllText(_logFile, $"\n{e.TimeStamp}\t{orderInfo}");
+        }
+    }
+}
+
