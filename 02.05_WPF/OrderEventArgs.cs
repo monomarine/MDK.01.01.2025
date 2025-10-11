655d70ece2210ab3a1b753299c764dd9c9a347d7
Azingamot <159550570+Azingamot@users.noreply.github.com>
2025-10-11 15:04:21 +0500

    Добавлено логгирование

---
+++ 02.05_WPF/OrderEventArgs.cs
@@ -0,0 +1,16 @@
+﻿namespace _02._05_Events
+{
+	internal class OrderEventArgs: EventArgs
+	{
+		public string Message { get;  }
+		public DateTime TimeStamp { get; }
+		public decimal Sum { get; }
+
+		public OrderEventArgs(string message, decimal sum)
+		{
+			this.Message = message;
+			this.Sum = sum;
+			this.TimeStamp = DateTime.Now;
+		}
+	}
+}
