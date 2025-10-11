b4515b29346a02fd8ce8ec714dc449950cdfbb06
cheriokok <rinaouko@mail.ru>
2025-10-11 15:34:25 +0500

    Яшина Кенгурова пк2

--- 02.05_EventsWPF/Data/FileAdd.cs
+++ 02.05_EventsWPF/Data/FileAdd.cs
@@ -1,25 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.IO;
 
 namespace _02._05_EventsWPF.Data
 {
-    public class NotificationService : IDisposable
+    public class FileAdd : IDisposable
     {
         private List<Order> _orders = new();
+        private string _logFile = "logs.txt";
 
         public event EventHandler<OrderEventArgs>? UpdateData;
         protected virtual void OnUpdateData(OrderEventArgs e)
         {
             UpdateData?.Invoke(this, e);
         }
-
         public void AddOrder(params Order[] orders)
         {
-            foreach(var o in orders)
+            foreach (var o in orders)
             {
                 o.Purchased += OrderPaid;
                 _orders.Add(o);
@@ -30,7 +30,7 @@ namespace _02._05_EventsWPF.Data
             foreach (var o in _orders)
                 o.Purchased -= OrderPaid;
 
-            _orders.Clear() ;
+            _orders.Clear();
         }
 
         public void OrderPaid(object? send, OrderEventArgs e)
@@ -38,12 +38,10 @@ namespace _02._05_EventsWPF.Data
             if (send is Order)
             {
                 var order = (Order)send;
-                string orderInfo = $"оплата от заказчика {order.Client} по заказу номер {order.Id} на сумму {e.Summ}";
                 OnUpdateData(new OrderEventArgs($"оплата от заказчика {order.Client}", e.Summ));
+                File.AppendAllText(_logFile, $"\n{e.TimeStamp}\t\"оплата от заказчика {order.Client} по заказу номер {order.Id} на сумму {e.Summ}");
             }
         }
-         
-
         public void Dispose()
         {
             CleanPublishers();
