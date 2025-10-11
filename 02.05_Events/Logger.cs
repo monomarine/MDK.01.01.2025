50864383233c8e7d06a39472a083f75cfa51aceb
Voxigen <Voxigen@HOME-PC>
2025-10-11 13:31:00 +0300

    c

---
+++ 02.05_Events/Logger.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02._05_Events
+{
+    internal class Logger
+    {
+        private readonly string _logFile;
+
+        public Logger(string logFile = "logs.txt")
+        {
+            _logFile = logFile;
+        }
+
+        public void Log(string message)
+        {
+            string fullMessage = $"{DateTime.Now:yyyy-MM-dd HH:mm:ss}\t{message}\n";
+            File.AppendAllText(_logFile, fullMessage);
+        }
+    }
+}
